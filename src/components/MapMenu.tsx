b892ddba1b6cbd69205c018452631463cd345a8a
WakkyCodes <wakky47@hotmail.com>
2025-10-03 00:45:21 +0200

    fix : removed the import

--- src/components/MapMenu.tsx
+++ src/components/MapMenu.tsx
@@ -1,14 +1,11 @@
-import { useGLTF } from "@react-three/drei"
+
 import MenuCar from "./MenuCar"
 
 export default function MapMenu() {
-  const { scene } = useGLTF(
-    `${import.meta.env.BASE_URL}models/mapmenu-draco.glb`
-  )
-
+  
   return (
     <>
-      <primitive object={scene} />
+      
       <MenuCar startPosition={[0, 0.5, 0]} />
     </>
   )
