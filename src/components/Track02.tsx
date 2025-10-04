5dbc5c76729a21acc1291dc71157b1941ef2277d
WakkyCodes <wakky47@hotmail.com>
2025-10-04 08:42:44 +0200

    added one stretch of walls

--- src/components/Track02.tsx
+++ src/components/Track02.tsx
@@ -5,13 +5,20 @@ export default function Track02() {
   const { scene } = useGLTF(`${import.meta.env.BASE_URL}models/track02-draco.glb`);
 
   // Define your wall boxes here
-  const wallBoxes: { position: [number, number, number]; scale: [number, number, number] }[] = [
+  const wallBoxes: { 
+    position: [number, number, number]; 
+    scale: [number, number, number] 
+    rotation?: [number, number, number];  
+  }[] = [
     { position: [7, 1, -8.5], scale: [30, 2, 0.5] },
-    { position: [37, 1, -8.5], scale: [30, 2, 0.5] }, // One long wall
+    { position: [37, 1, -8.5], scale: [30, 2, 0.5] },
+    { position: [62, 1, -11.5], scale: [20, 2, 1], rotation: [0, Math.PI / 10, 0] },  // One long wall
     { position: [7, 1, -6], scale: [30, 2, 0.5] },
     { position: [37, 1, -6], scale: [30, 2, 0.5] },
+    { position: [62, 1, -8.5], scale: [20, 2, 1], rotation: [0, Math.PI / 12, 0] },
+
+
     { position: [-15, 4, -26], scale: [1, 8, 5] },    // Tower collider
-    { position: [60, 1, -10], scale: [20, 2, 1] },     // Example wall
     { position: [10, 1, 9], scale: [1, 2, 20] },      // Example wall
     // Add more as needed
   ];
